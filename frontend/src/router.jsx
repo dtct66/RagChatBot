448bc5c8f435c8884442f8efc19399ab098d25df
dtct66 <t-daiki66@outlook.jp>
2025-03-27 16:39:17 +0900

    フロントエンドのチャット機能の作成

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import ChatPage from "./components/Chat/ChatPage";
 
 
 const AppRouter = () => {
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<ChatPage />} />
       </Routes>
     </Router>
   );
